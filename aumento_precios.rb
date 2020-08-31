9397091e421e33d1bd38bc6ca74b7971acbdfe69
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-31 19:26:28 -0400

    Corrected augment method and added its calling

--- aumento_precios.rb
+++ aumento_precios.rb
@@ -1,9 +1,13 @@
 def augment(arr, mult)
     new_arr = []
     arr.each do |e|
-        e*mult
-        new_arr.push(e)
+        new_arr.push(e*mult)
     end
     return new_arr
 end
+
+#Calling the method
+
+arr = augment([2, 3, 4, 5], 2)
+print "#{arr}\n" #Output should be [4, 6, 8, 10]
 
